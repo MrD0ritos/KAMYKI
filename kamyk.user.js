cb2e72138e37923f6c1882cc9a2ce8fa18e3bbc0
MrD0ritos <168548415+MrD0ritos@users.noreply.github.com>
2025-03-11 18:37:37 +0100

    Update kamyk.user.js

--- kamyk.user.js
+++ kamyk.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kamyki
 // @namespace    http://tampermonkey.net/
-// @version      0.21
+// @version      0.22
 // @description  try to take over the world!
 // @author       Slatya - edit by Mateoo, Pan Doritos
 // @match        *.margonem.pl/
